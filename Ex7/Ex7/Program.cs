843d025dfb1f5f849ae42dc1277759f750ceba5c
V1t0r_HuHn <104599280+VitorHuHn@users.noreply.github.com>
2026-05-11 21:22:49 -0300

    Add files via upload

---
+++ Ex7/Ex7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double milhas;
+            double milhasConv;
+            double conv;
+
+            conv = 1.852;
+
+            Console.Write("Digite o valor das milhas marítimas: ");
+            milhas = double.Parse(Console.ReadLine());
+
+            milhasConv = milhas * conv;
+
+            Console.Write("Essas milhas equivalem á {0} Km", milhasConv);
+
+        }
+    }
+}
